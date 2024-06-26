14ddbfbdbdd3b3e00e011231c462bfac8706711a
HrithikD9 <hrithikdevnath0924@gmail.com>
2024-06-26 11:43:20 +0600

    First

---
+++ Data_Structure/linkedlist/insertAtAnyPos.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+       this->data = data;
+       this->next = nullptr;
+    }
+};
+
+void insertAtTail(Node* &head , int data){
+    Node* newNode = new Node(data);
+
+    if(head == nullptr){
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+
+    while(temp->next != nullptr){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void insertAtAnyPos(Node* &head , int pos , int data){
+    Node* newNode = new Node(data);
+    Node* temp = head;
+
+    for(int i = 1; i <= pos - 1; i++){
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+    cout << endl;
+}
+
+void print(Node* head){
+    Node* temp = head;
+
+    while(temp != nullptr){
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main(){
+    Node* head = nullptr;
+
+    while (true){
+    cout << "Option 1 : Insert At Tail " << endl;
+    cout << "Option 2 : Print it !" << endl;
+    cout << "Option 3 : Insert at any position " << endl;
+    cout << "Option 4 : EXIT !" << endl;
+    cout << "Enter your choice : 1 / 2 / 3 / 4 : ";
+
+    int op;
+    cin >> op;
+    cout << endl;
+
+    if(op == 1){
+        cout << "Eneter data value for new node : ";
+        int data;
+        cin >> data;
+        cout << endl;
+        insertAtTail(head , data);
+    }
+
+    else if(op == 2){
+        cout << "The list are : ";
+        print(head);
+        cout << endl;
+    }
+
+    else if(op == 3){
+        cout << "Enter Position & Data Value : ";
+        int pos , data;
+        cin >> pos >> data;
+        insertAtAnyPos(head , pos , data);
+        cout << endl;
+    }
+
+    else if(op == 4){
+        break;
+    }
+
+    else{
+        cout << "Invalid";
+    }
+ }
+    return 0;
+}
