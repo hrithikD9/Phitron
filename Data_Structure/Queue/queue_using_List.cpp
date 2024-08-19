d89a9291df420bd4528d18a3b890eb3cc8b9feca
hrithikD9 <hrithikdevnath0924@gmail.com>
2024-08-19 21:08:16 +0600

    fire

---
+++ Data_Structure/Queue/queue_using_List.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class myQueue{
+    public:
+    list<int>l;
+
+    void push(int data){
+        l.push_back(data);
+    }
+
+    void pop(){
+        l.pop_front();
+    }
+
+    int front(){
+        return l.front();
+    }
+
+    int size(){
+        return l.size();
+    }
+
+    bool empty(){
+        return l.empty();
+    }
+
+};
+
+int main(){
+    myQueue q;
+
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+
+    while(!q.empty()){
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    return 0;
+}
